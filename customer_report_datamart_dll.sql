4b816806a8393d6ff0e628f534d75c03ee681c5c
Vova Kh <VKHDK@yandex.ru>
2024-05-06 19:06:23 +0300

    debug

--- customer_report_datamart_dll.sql
+++ customer_report_datamart_dll.sql
@@ -12,10 +12,11 @@ CREATE TABLE dwh.customer_report_datamart (
     median_month_order_lead_time_days int2 NOT NULL DEFAULT 0, -- Количество дней целое значение. Если клиент не совершал заказов в течении месяца, то медианное время выполнения заказа = 0
     top_month_product_category varchar NOT NULL DEFAULT 'No orders', -- Если клиент не совершал заказов в течении месяца, то категория продукта = 'No orders'
     top_craftsman_id varchar NOT NULL DEFAULT 'No orders', -- Если клиент не совершал заказов в течении месяца, то мастер = 'No orders'
-    count_month_order_created int8 NOT NULL, -- Если клиенты не совершали заказов в течении месяца, то количество созданных заказов = 0
+    count_month_order_created int8 NOT NULL DEFAULT 0, -- Если клиенты не совершали заказов в течении месяца, то количество созданных заказов = 0
 	count_month_order_in_progress int8 NOT NULL DEFAULT 0, -- Если клиенты не совершали заказов в течении месяца, то количество заказов в процессе выполнения = 0
 	count_month_order_delivery int8 NOT NULL DEFAULT 0, -- Если клиенты не совершали заказов в течении месяца, то количество заказов на доставке = 0
 	count_month_order_done int8 NOT NULL DEFAULT 0, -- Если клиенты не совершали заказов в течении месяца, то количество завершенных заказов = 0
 	count_month_order_not_done int8 NOT NULL DEFAULT 0, -- Если клиенты не совершали заказов в течении месяца, то количество не завершенных заказов = 0
     report_period date NOT NULL,
     CONSTRAINT customer_report_datamart_pk PRIMARY KEY (id)
+);
---
+++ load_dates_customer_report_datamart_dll.sql
@@ -0,0 +1,5 @@
+CREATE TABLE dwh.load_dates_customer_report_datamart (
+	id int8 GENERATED ALWAYS AS IDENTITY NOT NULL,
+	load_dttm date NOT NULL,
+	CONSTRAINT load_dates_customer_report_datamart_pk PRIMARY KEY (id)
+);
